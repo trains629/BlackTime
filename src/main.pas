0a2c59a57afec878fbe83dc84bcc4981de140d79
trains629 <trains629@163.com>
2013-11-30 16:58:09 +0800

    config.json src/main.pas

--- src/main.pas
+++ src/main.pas
@@ -16,6 +16,7 @@ type
     Fii,FMax,FMin,Fcenter : Integer;
     procedure setShow(cmdShow: Integer);
     function loadInit:Cardinal;
+    procedure SaveConfigWithInterval(configPath:string;Interval:Cardinal);
   public
     { Public declarations }
   end;
@@ -32,27 +33,44 @@ begin
   ShowWindow(Self.Handle,cmdShow);
 end;
 
+procedure TForm3.SaveConfigWithInterval(configPath:string;Interval:Cardinal);
+var
+  config : ISuperObject;
+begin
+  config := SO();
+  config.I['Interval'] := Interval;
+  config.I['Max'] := FMax;
+  config.I['Min']:= FMin;
+  config.SaveTo(configPath);
+end;
+
 function TForm3.loadInit():Cardinal;
 var
-  t1 :ISuperObject;
+  config:ISuperObject;
   path : string;
 begin
   Result := 1000;
-  FMax := 30;
-  FMin := -20;
+  FMax := 1800;
+  FMin := 10;
   Fcenter := 0;
-  path := ExtractFilePath(ParamStr(0))+'config.json';
-  if FileExists(path) then
-  begin
-    t1 := TSuperObject.ParseFile(path,True);
-    try
-    Result := t1.I['Interval'];
-    FMax   := t1.I['Max'];
-    FMin   := t1.I['Min'];
-    except
-    end;
+  try
+    path := ExtractFilePath(ParamStr(0))+'config.json';
+    if FileExists(path) then
+    begin
+      config := TSuperObject.ParseFile(path,True);
+      try
+        Result := config.I['Interval'];
+        FMax   := config.I['Max'];
+        FMin   := config.I['Min'];
+      except
+      end;
+    end
+    else
+      SaveConfigWithInterval(path,Result);
+  except
   end;
   Fii:= FMax;
+  FMin := -1 * FMin;
 end;
 
 procedure TForm3.FormCreate(Sender: TObject);
